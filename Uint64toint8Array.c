2bb422238c821f8514947f8f0f86ce3189943e66
tYin666 <tie.yin@gmail.com>
2021-08-29 20:30:38 +0200

    correct the algorithm Uint64toint8array.c

--- Uint64toint8Array.c
+++ Uint64toint8Array.c
@@ -5,14 +5,24 @@
 
 int main() 
 {
-    long long number = 0xFFFFFFFFFFFFFFFFF;
-    uint8 *p = (uint8_t *)&number;
+    long long number = 1234605616436508552;
     //if you need a copy
-    uint8 result[8];
-    for(int i = 0; i < 8; i++) {
-        result[i] = p[i];
-        printf("%d ", result[i]);
-        printf("\n");
-    }
-    return 0;
+    int byteArray[8];
+    byteArray[0] = (int)((number & 0xFF00000000000000) >> 56 );
+    byteArray[1] = (int)((number & 0x00FF000000000000) >> 48 );
+    byteArray[2] = (int)((number & 0x0000FF0000000000) >> 40 );
+    byteArray[3] = (int)((number & 0x000000FF00000000) >> 32 );
+    byteArray[4] = (int)((number & 0X00000000FF000000) >> 24);
+    byteArray[5] = (int)((number & 0x0000000000FF0000) >> 16 );
+    byteArray[6] = (int)((number & 0x000000000000FF00) >> 8 );
+    byteArray[7] = (int)((number & 0X00000000000000FF));
+    
+    printf("CHAR[0] 0x%02X\n",byteArray[0]);
+    printf("CHAR[1] 0x%02X\n",byteArray[1]);
+    printf("CHAR[2] 0x%02X\n",byteArray[2]);
+    printf("CHAR[3] 0x%02X\n",byteArray[3]);
+    printf("CHAR[4] 0x%02X\n",byteArray[4]);
+    printf("CHAR[5] 0x%02X\n",byteArray[5]);
+    printf("CHAR[6] 0x%02X\n",byteArray[6]);
+    printf("CHAR[7] 0x%02X\n",byteArray[7]);
 }
