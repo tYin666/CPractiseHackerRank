8daaaf60e1001619e192b2745533afe66884a2dd
tYin666 <tie.yin@gmail.com>
2021-09-18 23:11:40 +0200

    correct the put and get example

--- putandget.c
+++ putandget.c
@@ -6,7 +6,22 @@
 
 int main()
 {
- 
+    char catsName[50];
+    char catsFood[25];
+    char sentence[75];
+    
+    puts("whats the cats dumb name? ");
+    gets(catsName);
+
+    puts("whats does he eat? ");
+    gets(catsFood);
+
+    strcat(sentence, catsName);
+    strcat(sentence, " loves to eat ");
+    strcat(sentence, catsFood);
+
+    puts(sentence);    
+
 
     return 0;
 }
