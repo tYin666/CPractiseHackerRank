a44ef5b1564b3879b4959e0bc38e7f66b0b56284
tYin666 <tie.yin@gmail.com>
2021-09-19 22:03:02 +0200

    adjust the implmentation of intAndFloatArray.c

--- intAndFloatArray.c
+++ intAndFloatArray.c
@@ -6,8 +6,23 @@
 
 int main()
 {
-   
+   int i; 
+   int meatBalls[5]; 
+   int totalBalls = 0; 
 
+   for (i= 0; i<5; i++){
+       //printf("Eements %d: %d \n", i, meatBalls[i]);
+       printf("how many meatballs die you eat on day %d? \n", i+1);
+       scanf(" %d", &meatBalls[i]);
+   }
+
+   for (i = 0; i<5; i++){
+       totalBalls += meatBalls[i];
+   }
+12
+   int avg = totalBalls / 5; 
+
+   printf("You ate %d meatBalss total, thats an average of %d per day! \n",  totalBalls, avg);
 
     return 0;
 }
