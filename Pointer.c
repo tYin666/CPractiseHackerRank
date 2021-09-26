ce298733dc89a1ce3dced488b8eed8ab3544d70b
tYin666 <tie.yin@gmail.com>
2021-09-26 15:07:06 +0200

    implement the examples: point to a value  and point to an address

--- Pointer.c
+++ Pointer.c
@@ -6,5 +6,15 @@
 
 int main()
 {
+    int tuna = 19;
+    printf("Address \t\t Name\t Value \n");
+    //printf("%p \t %s \t %d \n", &tuna, "tuna", tuna);
+     
+    int * pTuna = &tuna;
+
+    printf("%p \t %s \t %d \n", pTuna, "tuna", tuna);
+
+    printf("%p \t %s \t %p \n", &pTuna, "pTuna", pTuna);
+
     return 0;
 }
