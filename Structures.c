f2528ba4d49e0a2b6a641c94ea83f4c1fd2699fa
tYin666 <tie.yin@gmail.com>
2021-09-28 22:14:41 +0200

    change the structure part

---
+++ Structures.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+// video 44: Point to a value and point to a address 
+// ArrayName save the address of first element
+int main()
+{
+  
+
+
+    return 0;
+}
