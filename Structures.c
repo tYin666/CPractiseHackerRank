2a3472710e2c062d04e24b46c9a5de1871f6eae0
tYin666 <tie.yin@gmail.com>
2021-10-04 21:22:16 +0200

    implement the example with sructure

--- Structures.c
+++ Structures.c
@@ -3,13 +3,25 @@
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
+#include "BuckysInfo.h"
 
 // video 44: Point to a value and point to a address 
 // ArrayName save the address of first element
 int main()
 {
-  
+   struct user bucky;
+   struct user emily; 
+   
+   bucky.userID = 10;
+   emily.userID = 2; 
 
+   puts("Enter the first name of user 1");
+   gets(bucky.firstName);
+   puts("Enter the first name of user 2");
+   gets(emily.firstName);
 
-    return 0;
+   printf("User 1 id is %d\n",  bucky.userID);
+   printf("User 2 firstname is %s\n",  emily.firstName);
+
+   return 0;
 }
